88fa6a41b077eb74ef3b82b30d8ee470e4c5c796
shyamriti <shyamritidas00@gmail.com>
2023-04-07 10:49:05 +0530

    comment out unnecessary struct

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,19 +1,14 @@
 package models
 
 type Person struct {
-
 	Id          string `json:"id"`
 	Name        string `json:"name"`
 	Email       string `json:"email"`
 	PhoneNumber string `json:"phone"`
-	
 }
 
-
-type JsonResponse struct {
-
-    Type    string `json:"type"`
-    Data    []Person `json:"data"`
-    Message string `json:"message"`
-
-}
+// type ResponseBody struct {
+// 	Type    string   `json:"type"`
+// 	Data    []Person `json:"data"`
+// 	Message string   `json:"message"`
+// }
